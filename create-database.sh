1e663c98823271eecc481c2da942f2c67383ae18
Johan Abbors <jabbors@gmail.com>
2021-11-06 22:44:06 +0200

    Setup influx database with retention policies and downsampling
    Additionally update the grafana dashboard to automatically select
    retention period according to selected time range.
    
    https://github.com/grafana/grafana/issues/4262#issuecomment-580509038

--- create-database.sh
+++ create-database.sh
@@ -5,5 +5,11 @@ docker-compose exec influxdb influx -execute "SHOW DATABASES" | grep "ruuvi" && 
 echo "creating database"
 docker-compose exec influxdb influx -execute "CREATE DATABASE ruuvi"
 docker-compose exec influxdb influx -execute "ALTER RETENTION POLICY \"autogen\" ON \"ruuvi\" DURATION 1h SHARD DURATION 30m"
-docker-compose exec influxdb influx -execute "CREATE RETENTION POLICY \"5m\" ON \"ruuvi\" DURATION 0s REPLICATION 1"
-docker-compose exec influxdb influx -execute "CREATE CONTINUOUS QUERY \"cq_basic_br\" ON \"ruuvi\" BEGIN SELECT mean(*) INTO \"ruuvi\".\"5m\".:MEASUREMENT FROM /.*/ GROUP BY time(5m),* END"
+docker-compose exec influxdb influx -execute "CREATE RETENTION POLICY \"1m\" ON \"ruuvi\" DURATION 24h REPLICATION 1"
+docker-compose exec influxdb influx -execute "CREATE RETENTION POLICY \"1h\" ON \"ruuvi\" DURATION INF REPLICATION 1"
+docker-compose exec influxdb influx -execute "CREATE CONTINUOUS QUERY \"1m_agg\" ON \"ruuvi\" BEGIN SELECT MEAN(\"temperature\") AS \"temperature\",MEAN(\"battery\") AS \"battery\",MEAN(\"humidity\") AS \"humidity\",MEAN(\"pressure\") AS \"pressure\",MEAN(\"rssi\") AS \"rssi\" INTO \"ruuvi\".\"1m\".\"reading\" FROM \"ruuvi\".\"autogen\".\"reading\" GROUP BY time(1m),sensor END"
+docker-compose exec influxdb influx -execute "CREATE CONTINUOUS QUERY \"1h_agg\" ON \"ruuvi\" BEGIN SELECT MEAN(\"temperature\") AS \"temperature\",MEAN(\"battery\") AS \"battery\",MEAN(\"humidity\") AS \"humidity\",MEAN(\"pressure\") AS \"pressure\",MEAN(\"rssi\") AS \"rssi\" INTO \"ruuvi\".\"1h\".\"reading\" FROM \"ruuvi\".\"1m\".\"reading\" GROUP BY time(1h),sensor END"
+docker-compose exec influxdb influx -execute "CREATE RETENTION POLICY \"grafana_rp\" ON \"ruuvi\" DURATION INF REPLICATION 1"
+docker-compose exec influxdb influx -execute "INSERT INTO grafana_rp config rp=\"autogen\",gb=\"1m\" 3600000000000"
+docker-compose exec influxdb influx -execute "INSERT INTO grafana_rp config rp=\"1m\",gb=\"1m\" 86400000000000"
+docker-compose exec influxdb influx -execute "INSERT INTO grafana_rp config rp=\"1h\",gb=\"1h\" 9223372036854775806" #-- max ns value in a 64-bit int
